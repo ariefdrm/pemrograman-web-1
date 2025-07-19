37fcea69b5473ae2877e1dd08d43f16250ac1d2a
ariefdrm <ariefdarmawan5522@gmail.com>
2025-07-19 10:33:18 +0700

    update: change navbar name into nim

--- MasterProject/resources/views/tugas.blade.php
+++ MasterProject/resources/views/tugas.blade.php
@@ -13,8 +13,8 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand text-light" href="#">Arief Darmawan 8020230033</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <a class="navbar-brand text-light" href="#">8020230033</a>
+            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
